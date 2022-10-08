d4e3d4d19337eb12b387c754126563006d7836c1
Soohyun Jung <sasha1107@naver.com>
2022-10-08 18:10:32 +0900

    Docs: 예시 코드  추가

---
+++ examples/interface.ts
@@ -0,0 +1,23 @@
+// 인터페이스 정의
+interface Menu {
+    name: string;
+    price: number;
+    onsale: boolean;
+}
+
+const americano: Menu = {
+    name: '아메리카노',
+    price: 4000, 
+    onsale: true
+}
+
+const lemonade: Menu = {
+    name: "레몬에이드",
+    price: 4500,
+    onsale: false
+}
+
+function Order(menu: Menu){
+    console.log(menu);
+}
+Order("cafelatte");
